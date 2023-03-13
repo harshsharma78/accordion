c7b71b28c9135a7302cee57cae611ee4e452e200
Harsh <namastecoding@gmail.com>
2023-03-13 15:42:59 +0530

    Accordion using react.js

---
+++ src/Question.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = ({ title, info }) => {
+	const [showInfo, setShowInfo] = useState(false);
+
+	const handleShowInfo = () => {
+		setShowInfo(!showInfo);
+	};
+
+	return (
+		<article className='question'>
+			<header>
+				<h4>{title}</h4>
+				<button
+					className='btn'
+					onClick={handleShowInfo}
+				>
+					{showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+				</button>
+			</header>
+			{showInfo && <p>{info}</p>}
+		</article>
+	);
+};
+
+export default Question;
